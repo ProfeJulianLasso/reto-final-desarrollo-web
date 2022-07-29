3bab86dcc42af34c299f2d8b2a8b5fe28719936e
JDQN <jdquimbayo72@misena.edu.co>
2022-07-29 13:01:45 -0500

    actualizacion proyect

--- backend/src/main/java/org/sofka/mykrello/MyKrelloApplication.java
+++ backend/src/main/java/org/sofka/mykrello/MyKrelloApplication.java
@@ -9,5 +9,5 @@ public class MyKrelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyKrelloApplication.class, args);
 	}
-
+//Talelr final
 }
--- backend/src/main/java/org/sofka/mykrello/model/domain/ColumnDomain.java
+++ backend/src/main/java/org/sofka/mykrello/model/domain/ColumnDomain.java
@@ -18,7 +18,6 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import lombok.Data;
 
 @Data
@@ -48,20 +47,24 @@ public class ColumnDomain implements Serializable {
     @Column(name = "clm_updated_at")
     private Instant updatedAt;
 
+
     @OneToMany(fetch = FetchType.LAZY, targetEntity = LogDomain.class, cascade = CascadeType.ALL, mappedBy = "previous")
     @JsonManagedReference(value = "logPrevious")
     @JsonIgnore
     private List<LogDomain> logPrevious = new ArrayList<>();
+
 
     @OneToMany(fetch = FetchType.LAZY, targetEntity = LogDomain.class, cascade = CascadeType.ALL, mappedBy = "current")
     @JsonManagedReference(value = "logCurrent")
     @JsonIgnore
     private List<LogDomain> logCurrent = new ArrayList<>();
 
+
     @OneToMany(fetch = FetchType.LAZY, targetEntity = ColumnForBoardDomain.class, cascade = CascadeType.ALL, mappedBy = "column")
     @JsonManagedReference(value = "columnForBoards")
     @JsonIgnore
     private List<ColumnForBoardDomain> columnForBoards = new ArrayList<>();
+
 
     @OneToMany(fetch = FetchType.LAZY, targetEntity = TaskDomain.class, cascade = CascadeType.ALL, mappedBy = "columnTask")
     //@JsonManagedReference(value = "taskForColumn")
